a83b9853779e3603cf79e830884ffa085aeb022c
Osiloke Emoekpere <osi@progwebtech.com>
2017-10-14 19:59:14 +0100

    add date filtering

--- common/query.go
+++ common/query.go
@@ -8,6 +8,20 @@ import (
 
 var logger = log.New("gostore-contrib.common")
 
+func formatted(prefix, field string, valRune []rune) (queryString string) {
+	v := strings.TrimSpace(string(valRune[1:]))
+	v = strings.Replace(v, "\"", "", -1)
+	switch strings.ToLower(string(valRune[0])) {
+	case "d":
+		logger.Debug("date format")
+		queryString = fmt.Sprintf(`+data.%s:%s"%v"`, field, prefix, v)
+	case "n":
+		queryString = fmt.Sprintf("+data.%s:%s=%v", field, prefix, v)
+	default:
+		queryString = fmt.Sprintf("+data.%s:%s%v", field, prefix, v) //this should not be supported
+	}
+	return strings.TrimSpace(queryString)
+}
 func GetQueryString(store string, filter map[string]interface{}) string {
 	queryString := "+bucket:" + store
 	for k, v := range filter {
@@ -16,17 +30,28 @@ func GetQueryString(store string, filter map[string]interface{}) string {
 			queryString = fmt.Sprintf("%s +data.%s:<=%v", queryString, k, _v)
 		} else if vv, ok := v.(string); ok {
 			valRune := []rune(vv)
-			first := string(valRune[0])
-			if first == "<" {
-				v = string(valRune[1:])
-				queryString = fmt.Sprintf("%s +data.%s:<=%v", queryString, k, v)
-			} else if first == ">" {
-				v = string(valRune[1:])
-				queryString = fmt.Sprintf("%s +data.%s:>=%v", queryString, k, v)
-
+			first := valRune[0]
+			if first == '\x3C' {
+				if valRune[1] == '\x25' {
+					// something like <%d2016-12-12
+					queryString = fmt.Sprintf("%s %s", queryString, formatted("<", k, valRune[2:]))
+				} else {
+					// something like <1
+					v = string(valRune[1:])
+					queryString = fmt.Sprintf("%s +data.%s:<=%v", queryString, k, v)
+				}
+			} else if first == '\x3E' {
+				if valRune[1] == '\x25' {
+					// something like >%d2016-12-12
+					queryString = fmt.Sprintf("%s %s", queryString, formatted(">", k, valRune[2:]))
+				} else {
+					// something like >20
+					v = string(valRune[1:])
+					queryString = fmt.Sprintf("%s +data.%s:>=%v", queryString, k, v)
+				}
 			} else {
 				prefix := "+"
-				if first == "!" {
+				if first == '\x21' {
 					prefix = "-"
 					v = string(valRune[1:])
 				}
@@ -36,5 +61,5 @@ func GetQueryString(store string, filter map[string]interface{}) string {
 			logger.Warn(store+" QueryString ["+k+"] was not parsed", "filter", filter, "value", v)
 		}
 	}
-	return strings.Replace(queryString, "\"", "", -1)
+	return queryString //strings.Replace(queryString, "\"", "", -1)
 }
